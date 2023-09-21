93fe6163fa040163c96a2636c316a70bbd37056b
CaiodelaRosa <caio.gomiero@hotmail.com>
2023-09-21 19:02:18 -0300

    adição de modal dos pokemons para mostrar detalhes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,49 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 const limit = 12
+
+
+
 let offset = 0
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map((pokemon) => `
-            <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.id}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
-                <img src="${pokemon.photo}"
-                alt="${pokemon.name}">
-                </div>
-            </li>`)
+        <a class="abrirModal ${pokemon.name}">    
+        <li class="pokemon ${pokemon.type}">         
+                <span class="number">#${pokemon.id}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        </ol>
+                    <img src="${pokemon.photo}"
+                    alt="${pokemon.name}">
+                    </div>
+                </li>
+            </a>
+        <dialog id="abrirModal ${pokemon.name}" class="pokeModal ${pokemon.name}">
+            <div>
+            <h2>${pokemon.name}</h2>
+            </div>
+        </dialog>`)
             .join('')
-        pokemonList.innerHTML += newHtml
-    })
+            pokemonList.innerHTML += newHtml;
+
+
+            const modalButton = document.getElementsByClassName("abrirModal")
+            const modalPokemon = document.getElementsByClassName("pokeModal")
+
+            for (let i = 0; i < modalButton.length; i++) {
+                modalButton[i].addEventListener('click', ()=>{
+                    modalPokemon[i].showModal()
+                })
+                
+              }
+                
+    })    
 }
+
 
 loadPokemonItens(offset, limit)
 
@@ -28,3 +51,4 @@ loadMoreButton.addEventListener('click', () => {
     offset += limit
     loadPokemonItens(offset, limit)
 })
+
